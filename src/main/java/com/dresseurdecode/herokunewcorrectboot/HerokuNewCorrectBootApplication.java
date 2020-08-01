f0b3d39889cae29d76524ea87a85568eaa5ce62e
MikeLogovi <40324123+MikeLogovi@users.noreply.github.com>
2020-08-02 00:45:50 +0100

    First commit

---
+++ src/main/java/com/dresseurdecode/herokunewcorrectboot/HerokuNewCorrectBootApplication.java
@@ -0,0 +1,13 @@
+package com.dresseurdecode.herokunewcorrectboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuNewCorrectBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuNewCorrectBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dresseurdecode/herokunewcorrectboot/HerokuNewCorrectBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dresseurdecode.herokunewcorrectboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuNewCorrectBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
